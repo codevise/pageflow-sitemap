e19062d5e5d7600e0b0ec8778909452aff51ccfa
Tim Fischbach <tfischbach@codevise.de>
2015-02-09 11:29:40 +0100

    Remove Model Specs

--- spec/javascripts/spec_helper.js
+++ spec/javascripts/spec_helper.js
@@ -2,33 +2,8 @@
 //= require support/chai
 //= require support/sinon-chai
 
-//= require support/factories
-
-//= require jquery
-//= require backbone-rails
-//= require i18n
-//= require i18n/translations
-
+//= require pageflow/base
 //= require pageflow/editor/base
-
-//= require pageflow/sitemap/editor.js
+//= require pageflow/sitemap/editor
 
 window.expect = chai.expect;
-
-chai.Assertion.addMethod('linkedTo', function (targetPage) {
-  var knob = this._obj;
-
-  this.assert(_.contains(knob.get('links').map(function (link) { return link.get('target'); }), targetPage),
-              knob + ' is linked to ' + targetPage,
-              knob + ' is not linked to ' + targetPage
-             );
-});
-
-chai.Assertion.addMethod('successorOf', function (predecessor) {
-  var successor  =this._obj;
-
-  this.assert(successor.get('predecessors').contains(predecessor) &&
-               predecessor.get('successor') === successor,
-               successor + ' is successor of ' + predecessor,
-               'Missing Link');
-});
