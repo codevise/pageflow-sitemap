c721af1453ba455a69e7d5183a7d06ee35c4940c
Tim Fischbach <tfischbach@codevise.de>
2015-02-23 15:29:47 +0100

    Explain layouting process in inline comments

--- app/assets/javascripts/pageflow/sitemap/editor/d3/layout.js
+++ app/assets/javascripts/pageflow/sitemap/editor/d3/layout.js
@@ -19,9 +19,14 @@
         grid: this.defaultGrid
       }, options || {});
 
+      // We begin by layouting chapters and pages in a grid.
+
       var originalLayout =
         new s.layout.Grid(pagesGroupedByChapters(entry),
                           options.grid);
+
+      // Then we create a layout in which the selected pages are
+      // translated by the dragDelta.
 
       var draggingLayout =
         new s.layout.DraggingDecorator(selection,
@@ -29,9 +34,17 @@
                                        originalLayout,
                                        {delta: options.dragDelta});
 
+      // When dragging pages, the reset of the layout collapses as if
+      // the dragged pages were removed. This is the layout we want to
+      // consider when we check collision with dragged pages.
+
       var collapsedLayout =
         new s.layout.Grid(draggingLayout.nonDraggedPagesGroupedByChapters,
                           options.grid);
+
+      // Group pages as if they already had been dropped into a target
+      // new chapters. This allows us to calculate a grid layout with
+      // free spaces where pages would be dropped.
 
       var collision =
         new s.layout.Collision(draggingLayout, collapsedLayout);
@@ -39,6 +52,13 @@
       var spacingLayout =
         new s.layout.Grid(collision.pagesGroupedByDragTargetChapters(),
                           options.grid);
+
+      // The final layout that we want to draw is split into two
+      // parts: For the selected pages, we want to use the position
+      // from the original layout translated by the dragDelta. For all
+      // other pages we want to use the positions from the spacing
+      // layout which already contains placeholder positions for the
+      // dragged pages.
 
       var layout =
         new s.layout.DraggingDecorator(selection,
