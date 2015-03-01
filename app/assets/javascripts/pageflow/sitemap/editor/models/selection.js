24c65e3059a6a2def4d9219fc65ae28bd69b23e6
Tim Fischbach <tfischbach@codevise.de>
2015-03-01 15:03:12 +0100

    Identify Page Links by id

--- app/assets/javascripts/pageflow/sitemap/editor/models/selection.js
+++ app/assets/javascripts/pageflow/sitemap/editor/models/selection.js
@@ -12,7 +12,8 @@ pageflow.sitemap.Selection = Backbone.Model.extend({
     return _(this.get('chapters')).contains(model) ||
       _(this.get('pages')).contains(model) ||
       _(this.get('pageLinks')).some(function(link) {
-        return link === model ||
+        return link === model |
+          (link.id && link.id === model.id) ||
           (link.placeholder && link.placeholder === model.placeholder);
       }) ||
       _(this.get('successorLinks')).some(function(link) {
