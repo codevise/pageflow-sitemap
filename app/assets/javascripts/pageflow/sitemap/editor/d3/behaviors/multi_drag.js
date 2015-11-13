fe959038f63ca7d0753ed838ff2e4b4c3b1ad107
Tim Fischbach <tfischbach@codevise.de>
2015-11-13 16:33:41 +0100

    Improve multi drag
    * Ensure there has at least been one dragmove event otherwise coords
      are invalid
    * Allow controlling whether dragging is enabled

--- app/assets/javascripts/pageflow/sitemap/editor/d3/behaviors/multi_drag.js
+++ app/assets/javascripts/pageflow/sitemap/editor/d3/behaviors/multi_drag.js
@@ -7,19 +7,28 @@ sitemap.behavior.multiDrag = function(options) {
 
   function behavior(g) {
     var handles = options.handle ? g.selectAll(options.handle) : g;
-    var dx, dy, x, y;
+    var dx, dy, x, y, isDragging, gotDragMove;
 
     var xxx;
 
     var drag = d3.behavior.drag()
-      .on(dragstart, function() {
+      .on(dragstart, function(data) {
         dx = 0;
         dy = 0;
+
+        isDragging = options.enabled ? options.enabled(data) : true;
+        gotDragMove = false;
 
         xxx = window.options.duration;
         window.options.duration = 0;
       })
       .on(dragmove, function() {
+        if (!isDragging) {
+          return;
+        }
+
+        gotDragMove = true;
+
         dx += d3.event.dx;
         dy += d3.event.dy;
 
@@ -37,15 +46,24 @@ sitemap.behavior.multiDrag = function(options) {
       })
       .on(dragend, function(data) {
         window.options.duration = xxx;
-        options.dragend({
-          data: data,
-          dx: dx,
-          dy: dy,
-          position: {
-            x: x,
-            y: y
-          }
-        });
+
+        if (!isDragging) {
+          return;
+        }
+
+        isDragging = false;
+
+        if (gotDragMove) {
+          options.dragend({
+            data: data,
+            dx: dx,
+            dy: dy,
+            position: {
+              x: x,
+              y: y
+            }
+          });
+        }
       });
 
     handles.call(drag);
