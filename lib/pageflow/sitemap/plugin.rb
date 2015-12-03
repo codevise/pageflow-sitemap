8fd15795ad79f7260177360775e85438d4117a08
Tim Fischbach <tfischbach@codevise.de>
2015-12-03 17:02:58 +0100

    Add help topic

--- lib/pageflow/sitemap/plugin.rb
+++ lib/pageflow/sitemap/plugin.rb
@@ -2,7 +2,9 @@ module Pageflow
   module Sitemap
     class Plugin < Pageflow::Plugin
       def configure(config)
-        config.features.register('sitemap')
+        config.features.register('sitemap') do |feature_config|
+          feature_config.help_entries.register('pageflow.sitemap.help_entries.main', priority: 6)
+        end
       end
     end
   end
